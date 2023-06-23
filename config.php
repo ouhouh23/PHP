c203dacb38aa936d81fe4047ac5b6cbd79630b9c
ouhouh23 <dexter32103210@gmail.com>
2023-06-23 12:47:35 +0300

    feat: database

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost',
+	'port' => 3306,
+	'dbname' => 'notes',
+	'charset' => 'utf8mb4'
+];
--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 
 require "functions.php";
 require "router.php";
+require "Database.php";
+
+$config = require 'config.php';
+
+$db = new Database($config);
+$id = $_GET['id'];
+$notes = $db->query("Select * from notes where id = ?", [$id])->fetch();
+dd($notes);
