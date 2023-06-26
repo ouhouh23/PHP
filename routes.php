7a0753d4098deac2278b91b1b45e599cce210132
Yury Andrush <dexter32103210@gmail.com>
2023-06-26 16:39:51 +0300

    refact: controllers

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ return [
 	'/' => 'controllers/index.php',
 	'/contact' => 'controllers/contact.php',
 	'/about' => 'controllers/about.php',
-	'/notes' => 'controllers/notes.php',
-	'/note' => 'controllers/note.php',
-	'/notes/create' => 'controllers/notes-create.php'
+	'/notes' => 'controllers/notes/index.php',
+	'/note' => 'controllers/notes/show.php',
+	'/notes/create' => 'controllers/notes/create.php'
 ];
